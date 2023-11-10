6938ad4cddc0dcb63498f30583591b8d2d8b0a37
MiravBhojani <95929083+MiravBhojani@users.noreply.github.com>
2023-11-10 21:11:57 +0300

    Change in code

--- project.R
+++ project.R
@@ -7,3 +7,10 @@ file_path <- "C:\\Users\\HP\\OneDrive - Strathmore University\\Strathmore\\Year 
 # Read the CSV file into a data frame
 Batting_data <- read_csv(file_path)
 
+# Install renv:
+if (!is.element("renv", installed.packages()[, 1])) {
+  install.packages("renv", dependencies = TRUE)
+}
+require("renv")
+# Select option 1 to restore the project from the lockfile
+renv::init()
