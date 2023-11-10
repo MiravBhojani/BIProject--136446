7b8ae822f5edd2c7189bc34b0c8122da5f0525f5
MiravBhojani <95929083+MiravBhojani@users.noreply.github.com>
2023-11-10 19:09:52 +0300

    loading the data

---
+++ project.R
@@ -0,0 +1,12 @@
+# Load the readr package
+library(readr)
+
+# Specify the file path with double backslashes
+file_path <- "C:\\Users\\HP\\OneDrive - Strathmore University\\Strathmore\\Year 4\\4.2\\Business Intelligence 2\\BIProject\\BIProject--136446\\data\\Batting data.csv"
+
+# Read the CSV file into a data frame
+Batting_data <- read_csv(file_path)
+
+# Display the first few rows of the data frame
+head(Batting_data)
+
