39cd3a94efdf380fea0254e245653d3ea214ea8f
MiravBhojani <95929083+MiravBhojani@users.noreply.github.com>
2023-11-11 09:25:11 +0300

    Addition in code

--- project.R
+++ project.R
@@ -1,9 +1,3 @@
-# Install and load the mlbench package
-if (!requireNamespace("mlbench", quietly = TRUE)) {
-  install.packages("mlbench")
-}
-library(mlbench)
-
 # Load the readr package
 library(readr)
 
@@ -13,8 +7,34 @@ file_path <- "C:\\Users\\HP\\OneDrive - Strathmore University\\Strathmore\\Year 
 # Read the CSV file into a data frame
 Batting_data <- read_csv(file_path)
 
-# Load the built-in dataset "Batting data" from mlbench
-data("Batting data")
-
 # Display the dimensions of the dataset
-dim(`Batting data`)
+dim(Batting_data)
+sapply(Batting_data, class)
+# Assuming "Righty/Lefty" is a categorical variable in your dataset
+righty_lefty_frequency <- table(Batting_data$`Righty or Lefty`)
+
+# Create a data frame with frequency and percentage
+frequency_table <- cbind(frequency = righty_lefty_frequency,
+                         percentage = prop.table(righty_lefty_frequency) * 100)
+
+# Print the frequency table
+print(frequency_table)
+
+# Mean
+mean_value <- mean(Batting_data$Runs)
+
+# Median
+median_value <- median(Batting_data$Runs)
+
+# Mode
+get_mode <- function(x) {
+  unique_x <- unique(x)
+  unique_x[which.max(tabulate(match(x, unique_x)))]
+}
+
+mode_value <- get_mode(Batting_data$Runs)
+
+# Display the results
+cat("Mean: ", mean_value, "\n")
+cat("Median: ", median_value, "\n")
+cat("Mode: ", mode_value, "\n")
